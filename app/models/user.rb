f3dfb3624bea849d9f8fffd01fd56eecb13bd274
stephchuolee <schuolee@gmail.com>
2019-04-18 21:13:44 -0700

    setup devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   enum role: [ :admin, :standard ]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 end
