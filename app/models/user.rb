7e4f8fa85369d4fd78789b576d401bb06ea56619
stephchuolee <schuolee@gmail.com>
2019-06-30 18:48:55 -0700

    change 'standard' user role to 'student'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  enum role: [ :admin, :standard ]
+  enum role: [ :admin, :student ]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
