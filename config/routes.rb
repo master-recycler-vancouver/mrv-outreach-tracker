73b234ed73d7ae3aa764ec629c101dd600e05398
Stavroula Theophanous <stavy92@hotmail.com>
2019-07-19 22:58:37 -0700

    added outreach_events to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users #, :controllers => { registrations: "registrations", sessions: "sessions", invitations: "invitations", password: "passwords" }
   root "home#index"
+
+  resources :outreach_events
 end
