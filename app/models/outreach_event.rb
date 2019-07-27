5b2a2bc8413e3f1a32c5e97a1fab5a0b4f9cf497
Stavroula Theophanous <stavy92@hotmail.com>
2019-07-26 21:25:44 -0700

    updated outreach event model: added virtual attr date_range, that has method that converts to start_time and end_time

--- app/models/outreach_event.rb
+++ app/models/outreach_event.rb
@@ -1,3 +1,16 @@
 class OutreachEvent < ApplicationRecord
+	DATE_FORMAT = "%m/%d/%Y"
+
   belongs_to :user
+
+  attr_accessor :date_range
+
+  def date_range=(val)
+  	start_str, end_str = val.split(' - ')
+
+  	p start_str
+
+  	self.start_time = Date.strptime(start_str, DATE_FORMAT)
+  	self.end_time = Date.strptime(end_str, DATE_FORMAT)
+	end
 end
