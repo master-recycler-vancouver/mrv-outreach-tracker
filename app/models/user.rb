ad8fb5fdc69e5f3f15fea3a64d880af2d211eab2
stephchuolee <schuolee@gmail.com>
2019-08-02 23:27:04 -0700

    remove registerable from User, minor styling to login form and nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   enum role: [ :admin, :student ]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   has_many :outreach_events
