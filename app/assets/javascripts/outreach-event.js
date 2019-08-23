41540d05e988cf2f30fb65d03597d16e514045c6
Stavroula Theophanous <stavy92@hotmail.com>
2019-08-23 12:54:06 -0700

    added labels for from and to dates

--- app/assets/javascripts/outreach-event.js
+++ app/assets/javascripts/outreach-event.js
@@ -6,7 +6,9 @@ function initDatepickers() {
   var calendarOptions = {
     isRange: true,
     minuteSteps: 15, //in case we ever use this for duration, 15 minute minimum,
-    dateFormat: "DD/MM/YYYY"
+    dateFormat: "DD/MM/YYYY",
+    labelFrom: "Start Date",
+    labelTo: "End Date"
   };
   // Initialize all input of type datetime
   var calendars = bulmaCalendar.attach('[type="datetime"]', calendarOptions);
