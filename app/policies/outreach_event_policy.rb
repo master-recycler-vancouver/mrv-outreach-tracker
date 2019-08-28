87902db8d2a98d5626753100d29cd9cbfbebcdda
Stavroula Theophanous <stavy92@hotmail.com>
2019-08-28 12:54:37 -0700

    admins allowed to edit outreach events

--- app/policies/outreach_event_policy.rb
+++ app/policies/outreach_event_policy.rb
@@ -9,7 +9,8 @@ class OutreachEventPolicy < ApplicationPolicy
   end
 
   def update?
-    user.student? && record.user == user
+    user.admin? || 
+    (user.student? && record.user == user)
   end
 
   def destroy?
