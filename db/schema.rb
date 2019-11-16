da843da9a9ccc5a0d1b4cba703d88a7120d5db7b
Stavroula Theophanous <stavy92@hotmail.com>
2019-11-15 16:32:37 -0800

    added title to outreach events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_191744) do
+ActiveRecord::Schema.define(version: 2019_11_15_205614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_191744) do
     t.integer "people_reached", null: false
     t.text "notes"
     t.bigint "user_id"
+    t.string "title"
     t.index ["user_id"], name: "index_outreach_events_on_user_id"
   end
 
