76e11896e76b610c3e9a92d4514cd8788c2e5c29
Stavroula Theophanous <stavy92@hotmail.com>
2019-12-27 13:20:44 -0500

    fixed issue with ransack search_form_for, solution found on the internet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  before_action :set_search
+
   protect_from_forgery
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -18,6 +20,10 @@ class ApplicationController < ActionController::Base
 
   def error_messages(obj)
     obj.errors.full_messages.join(", ")
+  end
+
+  def set_search
+    @q=User.search(params[:q])
   end
 
   private
