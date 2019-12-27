545fd9d574f25d2fe272ce7861e861744af5eb03
Stavroula Theophanous <stavy92@hotmail.com>
2019-12-27 13:45:58 -0500

    added redirect to outreach event index pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     skip_policy_scope
+
+    if current_user.admin?
+  		redirect_to admin_outreach_events_path
+		elsif current_user.facilitator?
+  		redirect_to facilitator_outreach_events_path
+		elsif current_user.student?
+  		redirect_to outreach_events_path
+		end
   end
 end
